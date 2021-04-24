c997d6ac56393cc607435e464a6534b917b3d348
ShahdQari <67500591+ShahdQari@users.noreply.github.com>
2021-04-25 01:16:47 +0300

    Add files via upload

--- Spatial index/src/operations/KNNQuery.java
+++ Spatial index/src/operations/KNNQuery.java
@@ -3,76 +3,100 @@ package operations;
 import core.Circle;
 import core.Point;
 import core.Rectangle;
-import java.awt.Graphics;
-import java.awt.geom.Ellipse2D;
-import static java.lang.Double.POSITIVE_INFINITY;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import operationsupport.CacheInfo;
+import operationsupport.OutputUtil;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 
 public class KNNQuery {
-    
-    public static void knnQuery(Point poi, Rectangle s,int k, Ignite ignite, String path,CacheInfo cacheInfo) {
-       Rectangle rect = new Rectangle();
-       int key = 0;
+
+    static Rectangle MBR ;;
+    static Rectangle rect;
+    public static void knnQuery(Point poi, int t, Rectangle s, int k, Ignite ignite, String path, CacheInfo cacheInfo) {
+        List<Point> point = new ArrayList<>();
+        
+        
+        int key = 0;
         IgniteCache<String, Rectangle> cache = ignite.cache("Rectangles");
         ArrayList<String> result = new ArrayList<String>();
         boolean found = true;
         while (found && key < cacheInfo.getQCount()) {
-             rect = cache.get("Cache---" + key);
+            rect = cache.get("Cache---" + key);
             if (rect.isIntersected(s)) {
                 result.add("Cache---" + key);
 
             } else {
                 //found=false;
             }
+            
             key++;
         }
-        ArrayList<Point> foundPoints = new ArrayList<Point>();
-        Circle circle; 
+        
+        List<Circle> list = new ArrayList<>();
         for (int i = 0; i < result.size(); i++) {
-            
             IgniteCache<Integer, ArrayList<Point>> pcache = cacheInfo.getCacheMap().get(result.get(i));
             ArrayList<Point> points = pcache.get(0);
             for (Point p : points) {
-                if (s.contains(p.x,p.y) ) {
-                    foundPoints.add(p);                    
+                if (s.contains(p.x, p.y)) {
+                    //&& p.day == 162  && p.type_task == 11
+                double distance = p.distanceTo(poi);
+                Circle circle = new Circle(p.x,p.y,distance*2);
+                list.add(circle);
+                Collections.sort(list, (a, b) -> b.compareTo(a));
+                Collections.reverse(list);
+                
                 }
-            }      
-        }
-        int K = 3 ;
-        Point p1 = new Point();
-        PriorityQueue<Double> list = new PriorityQueue<>();
-         for (Point p : foundPoints) {
-             double de = p.distanceTo(poi);
-            list.add(de);
-        }
-         int Key1 = 5 ; 
-         if(list.size() >= K){
-            double radius = list.peek();
-            circle = new Circle(p1.x , p1.y,radius*2);
-          
-            ArrayList<String> result_c = new ArrayList<String>();
-            while (found && key < cacheInfo.getQCount()) {
-             rect = cache.get("Cache---" + key);
-            if (circle.isIntersected(s)) {
-                result_c.add("Cache---" + key);
-                System.out.println(result_c.toString());
-
-            } else {
-                //found=false;
+                point.add(p);
+                
             }
-            Key1++;
+        }
+       // System.out.println(list.toString());
 
-         }
+
+
+    ArrayList<String> result_c = new ArrayList<String>();
+    for(int i = 3 ; i == k ; i++){
+       // if(i == k){
+          Circle circle = list.get(i);
+    //for(Circle circle : list.toArray(new Circle[list.indexOf(k)])){ 
+    
+         MBR = circle.getMBR();
+        int key1 = 0;
+        while (found && key1 < cacheInfo.getQCount()) { 
+            Rectangle getMBR = cache.get("Cache---" + key1);
+            if (MBR.isIntersected(getMBR)) {
+                //double distance = p.distanceTo(poi);
+                result_c.add("Cache---" + key1);   
+            }                    
+            key1++;
+        }
+    }
+   // }
+     //System.out.println(result_c.toString());
+     
+    ArrayList <String> knn = new ArrayList<>(); 
+          for (int i = 0; i < result_c.size(); i++) {
+            IgniteCache<Integer, ArrayList<Point>> pcache = cacheInfo.getCacheMap().get(result_c.get(i));
+            ArrayList<Point> points = pcache.get(0);
+            for (Point p : points) {
+                if (MBR.contains(p)) {
+                    //&& p.day == 162  && p.type_task == 11
+                double distance = p.distanceTo(poi);
+                Collections.sort(knn);  
+                knn.add(result_c.get(i) + "  distance: "+distance + " km" + "\n");
+
+                }  
+            }
+        }
+
+          System.out.println(knn.toString());
+          OutputUtil.knnquary(knn ,path, poi, cache, cacheInfo.getCacheMap());
+
     }
 }
-}
 
 
-
-
-    
